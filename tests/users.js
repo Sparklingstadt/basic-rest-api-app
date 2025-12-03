72aaebbcab9c4f5d2e58e362dd9d35f44148f964
sparklingstadt <sparklingstadt@outlook.jp>
2025-12-03 16:45:45 +0900

    test(api/users): add test draft

---
+++ tests/users.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * "/" ルート
+ * "Hello, world"
+ * 
+ * "/users" GET
+ * users一覧情報
+ * 
+ * "/users" POST
+ * - name: string
+ * 新規user
+ * 
+ * "/users/:id" GET
+ * 指定のuser || ユーザーなし
+ * 
+ * "/users/:id" PUT
+ * 更新された指定のuser || ユーザーなし
+ * 
+ * "/users/:id" DELETE
+ * 削除済み指定ユーザー || ユーザーなし
+ * 
+ * 
+ */
